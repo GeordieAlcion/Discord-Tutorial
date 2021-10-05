615206ced4a394a69e87b7a7fc2a5b311bd24d13
Geordie Ayers <geordie@MacBook-Pro.local>
2021-10-05 04:38:19 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import discord
+from dotenv import load_dotenv
+from os import getenv
+load_dotenv()
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('$thumb'):
+        channel = message.channel
+        await channel.send('Send me that 👍 reaction, mate')
+
+        def check(reaction, user):
+            return user == message.author and str(reaction.emoji) == '👍'
+
+        try:
+            reaction, user = await client.wait_for('reaction_add', timeout=60.0, check=check)
+        except asyncio.TimeoutError:
+            await channel.send('👎')
+        else:
+            await channel.send('👍')
+
+client.run(getenv('TOKEN'))
